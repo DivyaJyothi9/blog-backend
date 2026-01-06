e47d7052fea3adcb100c1c63daf03be6825df9c5
DivyaJyothi9 <liladivyajyothiakula@gmail.com>
2026-01-06 17:01:40 +0530

    Update backend for Render deployment (Procfile, requirements, dynamic port, db config)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from utils.db import db, users_collection
+import os
 
 # -------------------------
 #   CREATE FLASK APP
@@ -39,5 +40,9 @@ def test_db():
 # -------------------------
 #   START SERVER
 # -------------------------
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
